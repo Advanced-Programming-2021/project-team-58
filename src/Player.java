9cc6ae5c2f44471147cd1a0c85e4e0a2b37ad9db
Marzie <marzieafshar2002@yahoo.com>
2021-04-30 21:16:08 +0430

    add a new line

--- src/Player.java
+++ src/Player.java
@@ -10,6 +10,7 @@ public class Player {
     private int LP;
     private int money;
     private Board board;
+
     private ArrayList<Deck> decks;  //These are NOT the deck that is being used in the game
     private ArrayList<Card> hand;
 
