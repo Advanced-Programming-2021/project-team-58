f171e9ab7c3628750d55c9fdaa4d87300156e8a5
Marzie <marzieafshar2002@yahoo.com>
2021-05-05 16:12:37 +0430

    add some necessary methods to Deck.java

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -2,11 +2,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Deck {
-    private static ArrayList<Deck> allDecks;
-    private static HashMap<Player, Deck> playersAndTheirActivatedDeck;
+    private static ArrayList<Deck> allDecks = new ArrayList<Deck>();
+    private static HashMap<Player, Deck> playersAndTheirActivatedDeck = new HashMap<Player, Deck>();
     private String deckName;
-    private ArrayList<Card> mainDeck;
-    private ArrayList<Card> sideDeck;
+    private ArrayList<Card> mainDeck = new ArrayList<Card>();
+    private ArrayList<Card> sideDeck = new ArrayList<Card>();
+    private int deckSize;
+    private int mainDeckSize;
+    private int sideDeckSize;
 
     public Deck(String deckName, String playerName) {
         setDeckName(deckName);
@@ -22,10 +25,66 @@ public class Deck {
         return deckName;
     }
 
-    public static void addDeckToAllDecks(String deckName, String playerName){
-        allDecks.add(new Deck(deckName,playerName));
+    public int getDeckSize() {
+        return this.deckSize;
     }
-    public static void removeDeckFromAllDecks(String deckName){
+
+    public void increaseDeckSize(int howMany) {
+        this.deckSize += howMany;
+    }
+
+    public void decreaseDeckSize(int howMany) {
+        this.deckSize -= howMany;
+    }
+    public int getMainDeckSize() {
+        return this.mainDeckSize;
+    }
+
+    public void increaseMainDeckSize(int howMany) {
+        this.mainDeckSize += howMany;
+    }
+
+    public void decreaseMainDeckSize(int howMany) {
+        this.mainDeckSize -= howMany;
+    }
+
+    public int getSideDeckSize() {
+        return this.sideDeckSize;
+    }
+
+    public void increaseSideDeckSize(int howMany) {
+        this.sideDeckSize += howMany;
+    }
+
+    public void decreaseSideDeckSize(int howMany) {
+        this.sideDeckSize -= howMany;
+    }
+
+    public void addCardToMainDeck(Card card) {
+        this.mainDeck.add(card);
+        increaseMainDeckSize(1);
+    }
+
+    public void removeCardFromMainDeck(Card card) {
+        this.mainDeck.remove(card);
+        decreaseMainDeckSize(1);
+    }
+
+    public void addCardToSideDeck(Card card) {
+        this.sideDeck.add(card);
+        increaseSideDeckSize(1);
+    }
+
+    public void removeCardFromSideDeck(Card card) {
+        this.sideDeck.remove(card);
+        decreaseSideDeckSize(1);
+    }
+
+    public static void addDeckToAllDecks(String deckName, String playerName) {
+        allDecks.add(new Deck(deckName, playerName));
+    }
+
+    public static void removeDeckFromAllDecks(String deckName) {
         allDecks.remove(getDeckByName(deckName));
     }
 
