272fdd0104cf9f2bf01801b4700ce4c3cadb9472
Marzie <marzieafshar2002@yahoo.com>
2021-05-06 19:46:50 +0430

    add compareTo and isValid method

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,14 +1,15 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Deck {
+public class Deck implements Comparable<Deck> {
     private static ArrayList<Deck> allDecks = new ArrayList<Deck>();
     private static HashMap<Player, Deck> playersAndTheirActivatedDeck = new HashMap<Player, Deck>();
     private String deckName;
+    private boolean isValid;
     private ArrayList<Card> allCards = new ArrayList<Card>();
     private ArrayList<Card> mainDeck = new ArrayList<Card>();
     private ArrayList<Card> sideDeck = new ArrayList<Card>();
-    private HashMap<Card,Integer> cardsAndTheirAmountInThisDeck = new HashMap<Card, Integer>();
+    private HashMap<Card, Integer> cardsAndTheirAmountInThisDeck = new HashMap<Card, Integer>();
 
     public Deck(String deckName, String playerName) {
         setDeckName(deckName);
@@ -22,6 +23,11 @@ public class Deck {
 
     public String getDeckName() {
         return deckName;
+    }
+
+    public boolean isValid() {
+        if (this.mainDeck.size() >= 40 && this.mainDeck.size() <= 60 && this.sideDeck.size() <= 15) return true;
+        else return false;
     }
 
     public int getDeckSize() {
@@ -39,32 +45,32 @@ public class Deck {
     public void addCardToMainDeck(Card card) {
         this.mainDeck.add(card);
         this.allCards.add(card);
-        increaseNumOfCardInDeck(card,1);
+        increaseNumOfCardInDeck(card, 1);
     }
 
     public void removeCardFromMainDeck(Card card) {
         this.mainDeck.remove(card);
         this.allCards.remove(card);
-        decreaseNumOfCardInDeck(card,1);
+        decreaseNumOfCardInDeck(card, 1);
     }
 
     public void addCardToSideDeck(Card card) {
         this.sideDeck.add(card);
         this.allCards.add(card);
-        increaseNumOfCardInDeck(card,1);
+        increaseNumOfCardInDeck(card, 1);
     }
 
     public void removeCardFromSideDeck(Card card) {
         this.sideDeck.remove(card);
         this.allCards.remove(card);
-        decreaseNumOfCardInDeck(card,1);
+        decreaseNumOfCardInDeck(card, 1);
     }
 
     public static void addDeckToAllDecks(String deckName, String playerName) {
         allDecks.add(new Deck(deckName, playerName));
     }
 
-    public static void removeDeckFromAllDecks(String deckName){
+    public static void removeDeckFromAllDecks(String deckName) {
         allDecks.remove(getDeckByName(deckName));
     }
 
@@ -87,17 +93,19 @@ public class Deck {
         return cardsAndTheirAmountInThisDeck.get(card);
     }
 
-    public void increaseNumOfCardInDeck(Card card,int howMany) {
+    public void increaseNumOfCardInDeck(Card card, int howMany) {
         int theExistingNum = cardsAndTheirAmountInThisDeck.get(card);
         cardsAndTheirAmountInThisDeck.remove(card);
-        cardsAndTheirAmountInThisDeck.put(card,theExistingNum+howMany);
+        cardsAndTheirAmountInThisDeck.put(card, theExistingNum + howMany);
     }
 
     public void decreaseNumOfCardInDeck(Card card, int howMany) {
         int theExistingNum = cardsAndTheirAmountInThisDeck.get(card);
         cardsAndTheirAmountInThisDeck.remove(card);
-        cardsAndTheirAmountInThisDeck.put(card,theExistingNum-howMany);
+        cardsAndTheirAmountInThisDeck.put(card, theExistingNum - howMany);
     }
 
-
+    public int compareTo(Deck anotherDeck) {
+        return this.getDeckName().compareTo(anotherDeck.getDeckName());
+    }
 }
