4c12189416f9745a4572100ef029cbb085c4693c
bardia_rk <bardiarezaeikalantari@gmail.com>
2021-05-11 18:30:05 +0430

    add

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -5,37 +5,35 @@ public class Shop {
     public static void run() {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        Matcher matcher=getCommandMatcher(input, "shop buy ([A-Za-z]+)");
-        if (input.matches("shop buy ([A-Za-z]+)"))
+        Matcher matcher = null;
+        if (input.matches("shop buy ([A-Za-z]+)")){
             matcher = getCommandMatcher(input, "shop buy ([A-Za-z]+)");
-            if(matcher.find())
-        System.out.println(matcher.group(1));
-
-//        if (input.trim().equalsIgnoreCase("menu exit")) MainMenu.run();
-//        else while (!input.equalsIgnoreCase("menu exit")) {
-////            MonsterCard.addMonster();
-////            System.out.println(((MonsterCard)Card.getAllCards().get(0)).getAttack());
-//            if (getCommandMatcher(input.trim(), "^ [ ]+ menu enter [A-Za-z]+ [ ]+ $").find())
-//                System.out.println("menu navigation is not possible");
-//            if (input.trim().equalsIgnoreCase("menu show-current")) System.out.println("Shop");
-//
-//            if (getCommandMatcher(input.trim(), "shop buy ([A-Za-z]+)").find()) {
-//                if (checkCardExist(getCommandMatcher(input.trim(), "shop buy ([A-Za-z]+)").group(1)) &&
-//                        checkEnoughMoney(getCommandMatcher(input.trim(), "shop buy ([A-Za-z]+)").group(1)))
-//                    buy(getCommandMatcher(input.trim(), "shop buy ([A-Za-z]+)").group(1));
-//            }
-//            if (getCommandMatcher(input.trim(), "^ [ ]+ shop show --all [ ]+ $").find())
-//                showAllCards();
-//            input = scanner.nextLine();
-//        }
+            buy(getCommandMatcher(input, "shop buy ([A-Za-z]+)").group(1));
+        }
+        if (input.trim().equalsIgnoreCase("menu exit")) MainMenu.run();
+        else while (!input.equalsIgnoreCase("menu exit")) {
+            MonsterCard.addMonster();
+            if (getCommandMatcher(input.trim(), "^ [ ]+ menu enter [A-Za-z]+ [ ]+ $").find())
+                System.out.println("menu navigation is not possible");
+            if (input.trim().equalsIgnoreCase("menu show-current")) System.out.println("Shop");
+            if (getCommandMatcher(input.trim(), "shop buy ([A-Za-z]+)").find()) {
+                if (checkCardExist(getCommandMatcher(input.trim(), "shop buy ([A-Za-z]+)").group(1)) &&
+                        checkEnoughMoney(getCommandMatcher(input.trim(), "shop buy ([A-Za-z]+)").group(1)))
+                    buy(getCommandMatcher(input.trim(), "shop buy ([A-Za-z]+)").group(1));
+            }
+            if (getCommandMatcher(input.trim(), "^ [ ]+ shop show --all [ ]+ $").find())
+                showAllCards();
+            input = scanner.nextLine();
+        }
     }
 
     public static void buy(String cardName) {
         Player player = LoginMenu.getLoggedInPlayer();
         for (int i = 0; i < Card.getAllCards().size(); i++) {
             if (Card.getCardByName(cardName)!=null) {
-                player.decreaseMoney(Card.getCardByName(cardName).getPrice());
+                player.decreaseMoney(Objects.requireNonNull(Card.getCardByName(cardName)).getPrice());
                 player.getAllCards().add(Card.getCardByName(cardName));
+                System.out.println("successful");
             }
         }
     }
@@ -53,7 +51,8 @@ public class Shop {
 
             if (Card.getCardByName(cardName) != null) {
                 Player player = LoginMenu.getLoggedInPlayer();
-                if (Card.getCardByName(cardName).getPrice() > player.getMoney()) System.out.println("not enough money");
+                if (Objects.requireNonNull(Card.getCardByName(cardName)).getPrice() > player.getMoney())
+                    System.out.println("not enough money");
                 return false;
             }
 
@@ -62,7 +61,7 @@ public class Shop {
 
     public static void showAllCards() {
         for (int i = 0; i < Card.getAllCards().size(); i++) {
-            System.out.println(Card.getAllCards().get(i).getCardName() + ":" + Card.getAllCards().get(i).getPrice());
+            System.out.println(Card.getAllCards().get(i).getCardName() + " : " + Card.getAllCards().get(i).getPrice());
         }
     }
 
