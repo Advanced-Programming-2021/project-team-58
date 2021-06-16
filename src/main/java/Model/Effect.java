8403f41841317a1d1cdd47ee5768bba9491788f2
Marzie <79265056+marzieafshar@users.noreply.github.com>
2021-06-16 19:53:09 +0430

    Delete src directory

--- src/main/java/Model/Effect.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-import Controller.*;
-import View.*;
-
-abstract public class Effect {
-    abstract public void activate(Game game);
-    abstract public boolean isSuitableForActivate(Game game);
-}
